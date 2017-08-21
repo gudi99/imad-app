20ce05e048de48a6dfb379f0a82f6e42c4c29e8c
gudi99 <vnaykumr0@gmail.com>
2017-08-21 18:06:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/article-one', function (req, res) {
+app.get('/articleone', function (req, res) {
   res.sendFile('article is served here');
 });
 
